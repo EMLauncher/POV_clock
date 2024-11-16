11aa29a9cbd11498e6987c9955ddf69a435bd06f
EMLauncher <64531493+EMLauncher@users.noreply.github.com>
2024-11-16 11:56:15 +0900

    Add files via upload

---
+++ POV_v011/Release/Core/Src/draw_clock.d
@@ -0,0 +1,8 @@
+Core/Src/draw_clock.o: ../Core/Src/draw_clock.c ../Core/Inc/draw_clock.h \
+ ../Core/Inc/frame_pixel_data.csv ../Core/Inc/hour_pixel_data.csv \
+ ../Core/Inc/minute_pixel_data.csv ../Core/Inc/second_pixel_data.csv
+../Core/Inc/draw_clock.h:
+../Core/Inc/frame_pixel_data.csv:
+../Core/Inc/hour_pixel_data.csv:
+../Core/Inc/minute_pixel_data.csv:
+../Core/Inc/second_pixel_data.csv:
